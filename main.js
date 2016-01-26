0b5543c8d5541c477fbc94266a5443d03bac2d6e
Jasmine Guzman-Escobar <jasmineguzmanescobar@yahoo.com>
2016-01-26 12:59:18 -0800

    ttt model phil's way

--- main.js
+++ main.js
@@ -13,6 +13,21 @@ var board = [
   ];
 
 // 1. START THE GAME
+//    inputs:
+//      - current player, board, won
+//
+//    outputs:
+//      - board is emptied, player is X, won is false
+
+var startGame = function() {
+ won = false;
+ currentPlayer = "X";
+ board = [
+  "", "", "", // 0, 1, 2
+  "", "", "", // 3, 4, 5
+  "", "", ""  // 6, 7, 8
+  ];
+};
 // 2. MOVE (CHANGES TURN)
  //   inputs:
  //     - whose turn it is,
@@ -39,4 +54,39 @@ var move = function(cellIndex) {
 };
 
 // 3. WIN THE GAME
-var gameWon = function() {};
+  // inputs:
+  //   - board
+
+ //  outputs:
+ //   - true or false
+
+
+var gameWon = function() {
+ if (
+  ((board[0] === board[1]) && (board[0] === board[2]) && board[0] !== "") ||
+  ((board[3] === board[4]) && (board[3] === board[5]) && board[3] !== "") ||
+  ((board[6] === board[7]) && (board[6] === board[8]) && board[6] !== "")
+) {
+  return true;
+ } else {
+  return false;
+ }
+};
+
+
+// RENDER OUR VIEW //
+
+var render = function() {
+
+};
+
+
+
+
+
+
+
+
+
+
+
